93911f3d17ff51c48ed7b87093bea175c052d2ec
pablorg <pablo.romo.grilo.alu@iesfernandoaguilar.es>
2024-03-11 15:15:03 +0100

    commitPablo

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba{
 	public void main(String[] args){
 		System.out.println("Linea original");
 		System.out.println("Linea aniadida");
+		System.out.println("Linea aniadida Pablo");
 	}
 }
